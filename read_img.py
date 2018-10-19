3c6a7736dae1a8d3904c09b8d78840d1a10ebc93
Josefine Quack <Josefine Quack>
2018-10-20 00:20:34 +0200

    Image to text with pytesseract

---
+++ read_img.py
@@ -0,0 +1,9 @@
+import pytesseract
+from PIL import Image
+
+def image_recognize(inputpath, outputpath):
+    image = Image.open(inputpath)
+    text = pytesseract.image_to_string(image)
+    f = open(outputpath, "w+")
+    f.write(str(text))
+    f.close()
