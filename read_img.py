b520c303759acee87dd8e5802f5e24460f5be396
Josefine Quack <Josefine Quack>
2018-10-20 00:40:01 +0200

    Remove Bugs

--- read_img.py
+++ read_img.py
@@ -4,6 +4,7 @@ from PIL import Image
 def image_recognize(inputpath, outputpath):
     image = Image.open(inputpath)
     text = pytesseract.image_to_string(image)
+    print(text)
     f = open(outputpath, "w+")
     f.write(str(text))
     f.close()
