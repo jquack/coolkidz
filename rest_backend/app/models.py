9285ed8fcac55c19f3241f5c42668eb08db4c719
Olivier Cloux <olivier.cloux@epfl.ch>
2018-10-20 18:40:09 +0200

    GET in db written, not tested

--- rest_backend/app/models.py
+++ rest_backend/app/models.py
@@ -1,12 +1,44 @@
 from app import db
 
+
 class Boards(db.Model):
+    """A board, that is a list of expenses/tickets"""
     board_id = db.Column(db.Integer, primary_key=True)
-    board_address = db.Column(db.String, unique=True)
+    board_address = db.Column(db.String, unique=True, nullable=False)
+    users_list = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "Board {} at {} by {}".format(self.board_id, self.board_address, self.users_list)
+
 
 class Expenses(db.Model):
-    expense_id = db.Column(db.Integer, primary_key=True)
-    board_id = db.Column(db.Integer)
+    """An expense. This corresponds to 1 ticket"""
+    expense_id = db.Column(db.Integer(), primary_key=True)
+    board_id = db.Column(db.Integer, db.ForeignKey(
+        'boards.board_id'), nullable=False)
     amount = db.Column(db.Float)
-    payer = db.Column(db.String)
-    concerned_users = db.Column(db.String)
+    # "[(name, amount),...]"
+    payers = db.Column(db.String, nullable=False)
+    photo_filename = db.Column(db.String(50), unique=True)
+    date = db.Column(db.Date, nullable=False)
+
+    def __repr__(self):
+        return "{} paid {} on board {} for {}; see image {}".format(
+            self.payer, self.amount, self.board_id,
+            self.concerned_users, self.photo_filename)
+
+
+class Items(db.Model):
+    """One item on one ticket"""
+    item_id = db.Column(db.Integer(), primary_key=True)
+    expense_id = db.Column(db.Integer(), db.ForeignKey(
+        "expenses.expense_id"), nullable=False)
+    # name
+    item_name = db.Column(db.String(100), nullable=False, default="Unknown")
+    # price
+    item_price = db.Column(db.Float, nullable=False, default=0.0)
+    # [(name, quantity, amount),...]
+    concerned_users = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return "{} from expense {} at price {} for {}".format(self.item_name, self.expense_id, self.item_price, self.concerned_users.tostring())
--- rest_backend/read_img.py
+++ rest_backend/read_img.py
@@ -1,6 +1,7 @@
 import pytesseract
 from PIL import Image
 
+
 def image_recognize(inputpath):
     image = Image.open(inputpath)
     text = pytesseract.image_to_string(image)
@@ -10,15 +11,17 @@ def image_recognize(inputpath):
         price = None
         for word in row.split(" "):
             try:
-                price = float(word.replace(",","."))
+                price = float(word.replace(",", "."))
             except ValueError:
                 item += [word]
         if price:
             items_list += [(" ".join(item), price)]
     return items_list
-    
+
+
 def main():
     image_recognize("bills/bill1.jpg")
+
 
 if __name__ == "__main__":
     main()
